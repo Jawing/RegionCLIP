dfcbf4cd5a7c3a50ab94c488a315f4872629399f
wanjiz <ji-zou.wang@humanware.com>
2022-08-31 09:23:12 -0700

    metric switches

--- metrics.py
+++ metrics.py
@@ -12,18 +12,26 @@ gt_basicVal_json = './datasets/humanware/annotations/instances_val_basic.json'
 #result_lvis_json = './output/inference/lvis_instances_results.json'
 
 #define output coco json and ground true below
-result_coco_json = './output/inference/coco_instances_results_full_small_hw.json'
+Large = True
 basic = False
 val = False
-
+cf_percents = [0.99,0.98,0.97,0.96,0.95,0.94,0.93,0.92,0.91,0.9,0.85,0.8,0.75,0.7,0.5,0.25]
 
 if basic:
     if val:
         save_fig_loc = './output/inference/basicVal_pr50'
     else:
         save_fig_loc = './output/inference/basic_pr50'
+    if Large:
+        result_coco_json = './output/inference/coco_instances_results_full_large_basic.json'
+    else:
+        result_coco_json = './output/inference/coco_instances_results_full_small_basic.json'
 else:
     save_fig_loc = './output/inference/hw_pr50'
+    if Large:
+        result_coco_json = './output/inference/coco_instances_results_full_large_hw.json'
+    else:
+        result_coco_json = './output/inference/coco_instances_results_full_small_hw.json'
 
 classes = []
 
@@ -45,13 +53,17 @@ with open(result_coco_json) as fp:
 #get precision/recall/means
 gt_BoundingBoxes = get_bounding_boxes(gt_dataset)
 pd_BoundingBoxes = get_bounding_boxes(pred_dataset)
-results50,lrs_list_all = get_pascal_voc_metrics(gt_BoundingBoxes, pd_BoundingBoxes, 0.5, method= MethodAveragePrecision.AllPointsInterpolation)
+results50,lrs_list_all = get_pascal_voc_metrics(gt_BoundingBoxes, pd_BoundingBoxes, 0.5, 
+                    method= MethodAveragePrecision.AllPointsInterpolation,
+                    pr_percents= cf_percents)
 
 #get all mAP(0.5,0.05,0.95) and mean
 iou_list = np.arange(0.5,1.0,0.05)
 mAP_results_list = []
 for iou_thresh in iou_list:
-    results, _ = get_pascal_voc_metrics(gt_BoundingBoxes, pd_BoundingBoxes, iou_thresh, method= MethodAveragePrecision.AllPointsInterpolation)
+    results, _ = get_pascal_voc_metrics(gt_BoundingBoxes, pd_BoundingBoxes, iou_thresh,
+                     method= MethodAveragePrecision.AllPointsInterpolation,
+                     pr_percents= cf_percents)
     
     # #get results for each class
     # for cls, metric in results.items():
