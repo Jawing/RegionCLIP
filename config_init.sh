04d3174ba32d640f248ac9427e8507aa337679ae
wanjiz <ji-zou.wang@humanware.com>
2023-01-04 18:42:29 +0000

    server model and settings

--- config_init.sh
+++ config_init.sh
@@ -5,44 +5,47 @@
 
 # uncomment/comment to select between model before running server.py
 
-# for extra label classes, make sure to add/remove them from './labels_5class_c.txt' 
+# for text embeddings with extra label classes, make sure to add/remove relevant classes from './labels_5class_c.txt' 
 # with correct text embedding order before running server
 
 # #RN50 model trained on new basicAI keychain labels
-# python3 ./tools/config_init.py \
-# --eval-only  \
-# --num-gpus 1 \
-# --config-file ./configs/HUMANWARE-InstanceDetection/server_config.yaml \
-# MODEL.WEIGHTS ./models/model_best_s_110000_nkc.pth \
-# MODEL.CLIP.TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rclip_rn50_nn5.pth \
-# MODEL.CLIP.OPENSET_TEST_TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rclip_rn50_nn5.pth \
-# MODEL.CLIP.OFFLINE_RPN_CONFIG ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml \
-# MODEL.CLIP.BB_RPN_WEIGHTS ./pretrained_ckpt/rpn/rpn_coco_48.pth \
-
-#RN50 model trained on basicAI cup and pot labels
 python3 ./tools/config_init.py \
 --eval-only  \
 --num-gpus 1 \
---config-file ./configs/HUMANWARE-InstanceDetection/server_config_c.yaml \
-MODEL.WEIGHTS ./models/model_best_s_110000_cup_pot.pth \
-MODEL.CLIP.TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rn50_cup_pot.pth \
-MODEL.CLIP.OPENSET_TEST_TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rn50_cup_pot.pth \
+--config-file ./configs/HUMANWARE-InstanceDetection/server_config.yaml \
+MODEL.WEIGHTS ./models/model_best_s_110000_nkc.pth \
+MODEL.CLIP.TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rclip_rn50_nn5.pth \
+MODEL.CLIP.OPENSET_TEST_TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rclip_rn50_nn5.pth \
 MODEL.CLIP.OFFLINE_RPN_CONFIG ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml \
 MODEL.CLIP.BB_RPN_WEIGHTS ./pretrained_ckpt/rpn/rpn_coco_48.pth \
 
+#MODEL.CLIP.TEXT_EMB_PATH ./output/concept_feats/
+#concept_embeds_basic63_coco_86.pth
+#concept_embeds_basic_68.pth
+#concept_embeds_basic_coco.pth
+#concept_embeds_rclip_rn50_nn5.pth
 
-# #RN50 model trained on old basicAI keychain labels
-# python3 ./tools/config_init.py \
-# --eval-only  \
-# --num-gpus 1 \
-# --config-file ./configs/HUMANWARE-InstanceDetection/server_config.yaml \
-# MODEL.WEIGHTS ./models/model_best_small.pth \
-# MODEL.CLIP.TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rclip_rn50_nn5key.pth \
-# MODEL.CLIP.OPENSET_TEST_TEXT_EMB_PATH ./output/concept_feats/concept_embeds_rclip_rn50_nn5key.pth \
-# MODEL.CLIP.OFFLINE_RPN_CONFIG ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_C4_1x_ovd_FSD.yaml \
-# MODEL.CLIP.BB_RPN_WEIGHTS ./pretrained_ckpt/rpn/rpn_coco_48.pth \
+#MODEL.WEIGHTS ./models/
+#model_best_s_147500_nkc_rpn80.pth
+#model_best_s_65000_nkc_pretrained.pth
+#model_best_s_55000_basic63_LSJelevator.pth
+#model_best_s_40000_basic63_LSJ.pth
+#model_best_s_135000_basic63_coco_86.pth
+#model_best_s_22000_basic63.pth
+#model_best_s_132500_basic_coco.pth
+#model_best_s_16000_basic_LSJ_ADAM.pth
+#model_best_s_27000_basic_LSJ_SGD.pth
+#model_best_s_90000_nkc_lsj.pth
+#model_best_s_100_basic_boosted.pth
+#model_best_s_27500_basic.pth
+#model_best_s_50000_nkc_ubut.pth
+#model_best_s_7500_nkc_fbutfine.pth
+#model_best_s_77500_nkc_fbut.pth
+#model_best_s_25000_nkc_ubutfine.pth
+#model_best_s_110000_cup_pot.pth
+#model_best_s_110000_nkc.pth
 
-# #RN50x4 model trained on old basicAI keychain labels
+# #RN50x4 model inference trained on old basicAI keychain labels
 # python3 ./tools/config_init.py \
 # --eval-only  \
 # --num-gpus 1 \
